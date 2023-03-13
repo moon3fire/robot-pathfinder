4627fa5f8bdf08b4ebe3fc0e834ff084e8551284
Arman Isadzhanyan <moon3fire@mail.ru>
2023-03-13 21:44:12 +0400

    Added camera with needed features
    Camera can now rotate by circle of center of ground. UI will be added as soon as other parts of project will be done.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,20 +1,89 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public float zoomSpeed = 1f;
+    public float rotateSpeed = 1f;
+    public float pitchSpeed = 1f;
+    private float pitchAngle = 0f;
+    private float radius;
+    private bool isRotating = false;
+    private bool isPitching = false;
+
+    private Vector3 lastMousePosition;
+    private Vector3 center;
+    private Vector3 lastPitchMousePosition;
+
     [SerializeField]
-    private GameObject robot;
-    private Vector3 offset;
-    void Start()
+    private MeshFilter groundMeshFilter;
+
+    private void Start()
     {
-        offset = new Vector3(0, 10, -4);
+        center = groundMeshFilter.sharedMesh.bounds.center;
+        radius = Mathf.Sqrt(transform.position.y * transform.position.y + transform.position.z * transform.position.z);
     }
 
-    
-    void Update()
+    private void Update()
     {
-        gameObject.transform.position = robot.transform.position + offset;
+        ZoomInOut();
+        RotateCamera();
+        MoveCameraUpDown();
+        CorrectWithCircleCoordinates();
+    }
+
+    private void CorrectWithCircleCoordinates()
+    {
+        Vector3 newPosition = new Vector3(transform.position.x, 0f, transform.position.z).normalized * radius;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+
+    private void ZoomInOut()
+    {
+        float zoom = Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        transform.position += transform.forward * zoom;
+    }
+
+    private void RotateCamera()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isRotating = true;
+            lastMousePosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isRotating = false;
+        }
+
+        if (isRotating)
+        {
+            Vector3 deltaMousePosition = Input.mousePosition - lastMousePosition;
+            float angle = deltaMousePosition.x * rotateSpeed / radius;
+            transform.RotateAround(center, Vector3.up, angle);
+            lastMousePosition = Input.mousePosition;
+        }
+    }
+
+    private void MoveCameraUpDown()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            isPitching = true;
+            lastPitchMousePosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            isPitching = false;
+        }
+
+        if (isPitching)
+        {
+            Vector3 deltaMousePosition = Input.mousePosition - lastPitchMousePosition;
+            pitchAngle -= deltaMousePosition.y * pitchSpeed;
+            pitchAngle = Mathf.Clamp(pitchAngle, 0f, 70f);
+            transform.localRotation = Quaternion.Euler(pitchAngle, transform.localRotation.eulerAngles.y, transform.localRotation.eulerAngles.z);
+            lastPitchMousePosition = Input.mousePosition;
+        }
     }
 }
