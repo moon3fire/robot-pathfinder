72b33dcfccf953f0568ae9a36561541f7947aef6
Arman Isadzhanyan <moon3fire@mail.ru>
2023-03-16 06:36:01 +0400

    Corrected net generating code
    Now all border walls are included into the borders list which will help us to have only 1 solving path

--- Assets/Scripts/LabyrinthGenerator.cs
+++ Assets/Scripts/LabyrinthGenerator.cs
@@ -8,6 +8,11 @@ class LabyrinthGenerator : MonoBehaviour
     public int height = 10;
     private float wallLength = 10f;
 
+    private int spawnPosX;
+    private int spawnPosY;
+
+    [SerializeField]
+    private List<GameObject> borders;
    
     public GameObject wall0Prefab;
     public GameObject wall90Prefab;
@@ -15,8 +20,18 @@ class LabyrinthGenerator : MonoBehaviour
     // public GameObject[,] wallObjects;
     void Start()
     {
+        borders = new List<GameObject>();
         GenerateNet();
     }
+
+    void MakeNewPath()
+    {
+        spawnPosX = Random.Range(1, width);
+        spawnPosY = Random.Range(1, height);
+
+        //Will be continued
+    }
+
 
     void GenerateNet()
     {
@@ -24,23 +39,33 @@ class LabyrinthGenerator : MonoBehaviour
         {
             for (int j = 0; j < height; j++)
             {
-                Instantiate(wall0Prefab, new Vector3(i * wallLength + wallLength / 2f, 0, j * wallLength), Quaternion.identity);
+                GameObject temp1 = Instantiate(wall0Prefab, new Vector3(i * wallLength + wallLength / 2f, 0, j * wallLength), Quaternion.identity);
+                GameObject temp2 = Instantiate(wall90Prefab, new Vector3(i * wallLength, 0, j * wallLength + wallLength / 2f), Quaternion.Euler(0, 90, 0));
+                temp1.name = "Wall_" + i + "_" + j;
+                temp2.name = "Wall_" + j + "_" + i;
+                if(i != 0 && j == 0 || i == 0 && j == 0)
+                {
+                    borders.Add(temp1);
+                }
+                if(j != 0 && i == 0 || j == 0 && i == 0)
+                {
+                    borders.Add(temp2);
+                }
             }
         }
 
         for (int i = 0; i < width; i++)
         {
-            for (int j = 0; j < height; j++)
-            {
-                Instantiate(wall90Prefab, new Vector3(i * wallLength, 0, j * wallLength + wallLength / 2f), Quaternion.Euler(0, 90, 0));
-            }
+            GameObject temp = Instantiate(wall0Prefab, new Vector3((i + 0.5f) * wallLength, 0, (height - 0.5f) * wallLength + wallLength / 2f), Quaternion.identity);
+            temp.name = "Wall_" + i + "_width_end";
+            borders.Add(temp);
         }
 
-        for (int i = 0; i < width; i++)
+        for(int i = 0; i < height; i++)
         {
-            //NOTE:: THIS GONNA WORK ONLY WHEN WIDTH = HEIGHT :: NEED TO BE CHANGED LATER INTO 2 FOR'S
-            Instantiate(wall0Prefab, new Vector3((i + 0.5f) * wallLength, 0, (height - 0.5f) * wallLength + wallLength / 2f), Quaternion.identity);
-            Instantiate(wall90Prefab, new Vector3((width - 0.5f) * wallLength + wallLength / 2f, 0, (i + 0.5f) * wallLength), Quaternion.Euler(0, 90, 0));
+            GameObject temp = Instantiate(wall90Prefab, new Vector3((width - 0.5f) * wallLength + wallLength / 2f, 0, (i + 0.5f) * wallLength), Quaternion.Euler(0, 90, 0));
+            temp.name = "Wall_" + i + "_height_end";
+            borders.Add(temp);
         }
     }
 
